2575cefe7f2a0d4d1425bcbb70bc860e39b3f095
Priya Sundararaj <itspria@gmail.com>
2021-10-16 19:40:54 -0400

    Added the model code to app.py

--- models.py
+++ models.py
@@ -1,12 +1,25 @@
+from sqlalchemy.ext.declarative import declarative_base
+Base= declarative_base()
+
 def create_classes(db):
-    class Pet(db.Model):
-        __tablename__ = 'pets'
+    class Test(db.Model):
+        __tablename__ = 'test'
 
         id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String(64))
-        lat = db.Column(db.Float)
-        lon = db.Column(db.Float)
-
+        name = db.Column(db.String(50))        
+        print ("Model Name ",name)
         def __repr__(self):
-            return '<Pet %r>' % (self.name)
-    return Pet
+            return '<Test %r>' % (self.name)
+    return Test
+
+def create_rating_class(db):
+    class Ratings(db.Model):
+        __tablename__ = 'ratings'
+
+        movie_id = db.Column(db.Integer(), primary_key=True)
+        userId = db.Column(db.Integer())
+        rating = db.Column(db.Integer())
+    
+        def __repr__(self):
+            return '<Ratings %r>' % (self.name)
+    return Ratings
